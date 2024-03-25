1be0cd0514a7a27322cace6062a230e42a2b3369
Arowo <Arowobusoyesaanu088@gmail.com>
2024-03-25 04:01:48 -0700

    1-create_dynamic_lib.sh,libdynamic.so, main.h

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+        #define EXIT_SUCCESS 0
+        return (EXIT_SUCCESS);
+}
