c3f082779e451dee360862df793ff636cea2d513
Arowo <Arowobusoyesaanu088@gmail.com>
2024-04-02 07:43:59 -0700

    the 2nd commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    sum = 0
     try:
+        count = 0
         for i in range(x):
-            sum += 1
-            print(my_list[i], end='')
-        print()
+            print(my_list[i], end="")
+            count += 1
     except IndexError:
-        sum -= 1
-        print()
         pass
-    return sum
+    finally:
+        print()
+        return count
